b4382b7b8b1eafe92e4e367d489d6e0e9567000d
AnSui22 <ansui218@ewhain.net>
2024-10-27 22:55:29 +0900

    Feat: 기본 리액트 앱 설정 및 라우터 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Recommendation from "./pages/Recommendation";
+import Start from "./pages/Start";
+import Test from "./pages/Test";
+import Loading from "./pages/Loading";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/start" element={<Start />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/recommendation" element={<Recommendation />} />
+        <Route path="/loading" element={<Loading />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
